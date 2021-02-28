45f91e039c611d87b868c49a795bd4255d7510a5
Amir Mairaj <75645123+amiramadmairaj@users.noreply.github.com>
2021-02-28 15:52:23 -0800

    Add files via upload

---
+++ Lab_3/mem.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <malloc.h>
+
+ int main() {
+	int num;
+	int *ptr;
+	int **handle;
+
+	num = 14;
+	ptr = (int *)malloc(2 * sizeof(int));
+	*ptr = num;
+	handle = (int **)malloc(1 * sizeof(int *));
+	*handle = ptr;
+
+	//Insert extra code here
+	//Variable Addresses:
+	printf("Address of num: %p \n", &num);
+	printf("Address of ptr: %p \n", &ptr);
+	printf("Address of handle: %p \n", &handle );
+	printf("Address of *ptr: %p \n", &*ptr );
+	printf("Address of *handle: %p \n", *handle );
+
+
+	return 0;
+} 
+
